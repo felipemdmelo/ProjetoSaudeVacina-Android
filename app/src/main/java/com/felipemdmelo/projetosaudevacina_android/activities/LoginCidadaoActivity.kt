c0f982902e9c789a6e5ec05143bf1c20f2e0359f
felipe.melo@urjasocial.com.br <felipe.melo@urjasocial.com.br>
2018-09-13 18:34:47 -0300

    Adicionando tela de login

---
+++ app/src/main/java/com/felipemdmelo/projetosaudevacina_android/activities/LoginCidadaoActivity.kt
@@ -0,0 +1,13 @@
+package com.felipemdmelo.projetosaudevacina_android.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.felipemdmelo.projetosaudevacina_android.R
+
+class LoginCidadaoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_cidadao)
+    }
+}
